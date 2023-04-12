0ccaf2709c265a96b72691fc13b8acd9b560d5fd
Tulio <tulioruzo29@gmail.com>
2023-04-12 15:10:28 -0400

    Create Projects Section

---
+++ src/components/Projects/ProjectCard.tsx
@@ -0,0 +1,21 @@
+import { Col } from "react-bootstrap";
+
+type Props = {
+   title: string;
+   description: string;
+   imgUrl: any;
+};
+
+export default function ProjectCard({ title, description, imgUrl }: Props) {
+   return (
+      <Col size={12} sm={6} md={4}>
+         <div className="proj-imgbx">
+            <img src={imgUrl} alt={title} />
+            <div className="proj-txtx">
+               <h4>{title}</h4>
+               <span>{description}</span>
+            </div>
+         </div>
+      </Col>
+   );
+}

880be81ea686356758b42a1ce658d57e7060a756
Tulio <tulioruzo29@gmail.com>
2023-04-12 14:29:04 -0400

    Create Skills Sections

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -6,23 +6,12 @@ import "animate.css";
 import TrackVisibility from "react-on-screen";
 
 export default function Banner() {
-   const [loopNum, setLoopNum] = useState(0);
-   const [isDeleting, setIsDeleting] = useState(false);
-   const [text, setText] = useState("");
-   const [delta, setDelta] = useState(300 - Math.random() * 100);
-   const [index, setIndex] = useState(1);
+   const [loopNum, setLoopNum] = useState<number>(0);
+   const [isDeleting, setIsDeleting] = useState<boolean>(false);
    const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+   const [text, setText] = useState<string>("");
+   const [delta, setDelta] = useState<number>(300 - Math.random() * 100);
    const period = 2000;
-
-   useEffect(() => {
-      let ticker = setInterval(() => {
-         tick();
-      }, delta);
-
-      return () => {
-         clearInterval(ticker);
-      };
-   }, [text]);
 
    const tick = () => {
       let i = loopNum % toRotate.length;
@@ -39,17 +28,24 @@ export default function Banner() {
 
       if (!isDeleting && updatedText === fullText) {
          setIsDeleting(true);
-         setIndex((prevIndex) => prevIndex - 1);
          setDelta(period);
       } else if (isDeleting && updatedText === "") {
          setIsDeleting(false);
          setLoopNum(loopNum + 1);
-         setIndex(1);
          setDelta(500);
-      } else {
-         setIndex((prevIndex) => prevIndex + 1);
       }
    };
+
+   useEffect(() => {
+      let ticker = setInterval(() => {
+         tick();
+      }, delta);
+
+      return () => {
+         clearInterval(ticker);
+      };
+      // eslint-disable-next-line
+   }, [text, delta]);
 
    return (
       <section className="banner" id="home">

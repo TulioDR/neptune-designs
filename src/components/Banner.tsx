19e866106d7cb0b6585040fc6b29d9284ad954df
Tulio <tulioruzo29@gmail.com>
2023-04-12 22:09:42 -0400

    Remove react-on-screen package

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,8 +2,6 @@ import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import "animate.css";
-import TrackVisibility from "react-on-screen";
 
 export default function Banner() {
    const [loopNum, setLoopNum] = useState<number>(0);
@@ -52,51 +50,30 @@ export default function Banner() {
          <Container>
             <Row className="align-items-center">
                <Col xs={12} md={6} xl={7}>
-                  <TrackVisibility>
-                     {({ isVisible }) => (
-                        <div
-                           className={
-                              isVisible
-                                 ? "animate__animated animate__fadeIn"
-                                 : ""
-                           }
-                        >
-                           <span className="tagline">Welcome to Neptune</span>
-                           <h1>
-                              {`We`}{" "}
-                              <span className="txt-rotate">
-                                 <span className="wrap">{text}</span>
-                              </span>
-                           </h1>
-                           <p>
-                              Lorem Ipsum is simply dummy text of the printing
-                              and typesetting industry. Lorem Ipsum has been the
-                              industry's standard dummy text ever since the
-                              1500s, when an unknown printer took a galley of
-                              type and scrambled it to make a type specimen
-                              book.
-                           </p>
-                           <button onClick={() => console.log("connect")}>
-                              Let’s Connect <ArrowRightCircle size={25} />
-                           </button>
-                        </div>
-                     )}
-                  </TrackVisibility>
+                  <div className="animate__animated animate__fadeIn">
+                     <span className="tagline">Welcome to Neptune</span>
+                     <h1>
+                        {`We`}{" "}
+                        <span className="txt-rotate">
+                           <span className="wrap">{text}</span>
+                        </span>
+                     </h1>
+                     <p>
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book.
+                     </p>
+                     <button onClick={() => console.log("connect")}>
+                        Let’s Connect <ArrowRightCircle size={25} />
+                     </button>
+                  </div>
                </Col>
                <Col xs={12} md={6} xl={5}>
-                  <TrackVisibility>
-                     {({ isVisible }) => (
-                        <div
-                           className={
-                              isVisible
-                                 ? "animate__animated animate__zoomIn"
-                                 : ""
-                           }
-                        >
-                           <img src={headerImg} alt="Header Img" />
-                        </div>
-                     )}
-                  </TrackVisibility>
+                  <div className="animate__animated animate__zoomIn">
+                     <img src={headerImg} alt="Header Img" />
+                  </div>
                </Col>
             </Row>
          </Container>

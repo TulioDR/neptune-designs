6ef7c016f6d97ee65a8676127d22fcadb91c5ef5
Tulio <tulioruzo29@gmail.com>
2023-04-13 18:46:49 -0400

    Update Projects text

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -15,11 +15,11 @@ export default function Projects() {
                   <div className="animate__animated animate__fadeIn">
                      <h2>Projects</h2>
                      <p>
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book.
+                        From personal portfolios, through small businesses
+                        websites, to complex applications for big companies. We
+                        cover them all.
+                        <br />
+                        Here are some examples for you to review.
                      </p>
                      <Tab.Container id="projects-tabs" defaultActiveKey="first">
                         <Nav
